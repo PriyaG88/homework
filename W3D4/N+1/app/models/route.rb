65ceb7759a62f54abfdf4704e4f8bfe8d47bd27a
PriyaG88 <priya.g81488@gmail.com>
2017-08-17 09:19:42 -0700

    Implementation of faster query searches on House, Artist and Route

---
+++ W3D4/N+1/app/models/route.rb
@@ -0,0 +1,44 @@
+class Route < ApplicationRecord
+  has_many :buses,
+    class_name: 'Bus',
+    foreign_key: :route_id,
+    primary_key: :id
+
+  def n_plus_one_drivers
+    buses = self.buses
+
+    all_drivers = {}
+    buses.each do |bus|
+      drivers = []
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+
+    all_drivers
+  end
+
+  def better_drivers_query
+    # TODO: your code here
+    buses = self.buses.includes(:drivers)
+    all_drivers = {}
+
+    buses.each do |bus|
+      drivers = []
+      drivers << driver.name
+
+      bus.drivers.each do |driver|
+        drivers << driver.name
+      end
+      all_drivers[bus.id] = drivers
+    end
+    all_drivers
+  end
+end
+
+# posts = self.posts.includes(:comments)
+# post_comment_counts = {}
+#     posts.each do |post|
+#       post_comment_counts[post] = post.comments.length
+#     end
